e565d6ef6f6d8bb53a8e76619d0fcd9741f08f85
atagle123 <119713621+atagle123@users.noreply.github.com>
2023-12-11 14:17:24 -0300

    Delete data directory

--- data/c_slope_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-px = np.array([[0.25, 0.25, 0.25,0.25]]).T #  distribucion de x M=4  (M,1)
-py = np.array([0.25, 0.25, 0.25,0.25]).T # K= 4  distribucion de y   (K,1)
-py_x = 1/2*np.array([[1, 1, 0, 0],
-                [1, 1, 0, 0],
-                [0, 0, 1, 1],
-                [0, 0, 1, 1]])
